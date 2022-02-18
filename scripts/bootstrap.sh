3115ca603e396981680b8a7cbe5f0759cb06e5e2
Alexander Demidovskij <alexander.demidovskij@intel.com>
2022-02-18 20:19:11 +0300

    bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,19 @@
+#apt -y update
+#apt -y upgrade
+#apt install -y git
+#git clone https://github.com/dl-wb-experiments/hugging-face-exploration
+#cd hugging-face-exploration
+#bash scripts/bootstrap.sh
+
 apt -y update
 apt -y upgrade
 apt install -y --no-install-recommends \
         libgl1 \
         libglib2.0-0 \
         htop
-        git
+
+python3 -m pip install virtualenv
+python3 -m virtualenv venv
+source venv/bin/activate
+
+python -m pip install -r requirements.txt
