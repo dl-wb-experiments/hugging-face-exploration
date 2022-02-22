531c61c7c7575b8703fcf1647fb86ab86e71f232
Artur Paniukov <artur.paniukov@intel.com>
2022-02-22 18:27:24 +0300

    Avoid warning from tokenizers

--- scripts/run_validation.sh
+++ scripts/run_validation.sh
@@ -3,6 +3,8 @@ source venv/bin/activate
 rm -rf logs
 mkdir -p logs
 
+export TOKENIZERS_PARALLELISM=false
+
 nohup python -u validation_pipeline_mp.py \
                 > logs/validation_pipeline_mp.log \
                 2> logs/validation_pipeline_mp_err.log &
