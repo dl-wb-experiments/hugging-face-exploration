371eade8c6d6b9cf5d3b5a4950724cc852648a28
Alexander Demidovskij <alexander.demidovskij@intel.com>
2022-02-22 21:03:55 +0300

    better copying logic

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,13 +1,25 @@
 # make the script verbose
-set -x
+set -e
 
 WHERE_TO_KEEP=~/Downloads/new_report/
-#WHAT_TO_KEEP=/root/hugging-face-exploration/logs/onnx_to_openvino_mp.log
-WHAT_TO_KEEP=/root/hugging-face-exploration/reports/cumulative_onnx_to_ir.json
-#WHAT_TO_KEEP=/root/hugging-face-exploration/reports/hf_to_onnx.json
 SERVER_IP=remote-alexander
+DO_CLEANUP=1
 
-#rm -rf ${WHERE_TO_KEEP}
-#mkdir -p ${WHERE_TO_KEEP}
+if [ "${DO_CLEANUP}" == "1" ]; then
+  echo "Cleaning the system ..."
+  rm -rf ${WHERE_TO_KEEP}
+  mkdir -p ${WHERE_TO_KEEP}
+fi
 
-scp -i ~/.ssh/id_rsa_ya -r remote-alexander:${WHAT_TO_KEEP} ${WHERE_TO_KEEP}
+
+FILES_TO_COPY=(
+  '/root/hugging-face-exploration/logs/onnx_to_openvino_mp.log'
+  '/root/hugging-face-exploration/reports/cumulative_onnx_to_ir.json'
+  '/root/hugging-face-exploration/reports/hf_to_onnx.json'
+)
+
+for remote_file_path in "${FILES_TO_COPY[@]}"
+do
+  echo "downloading ${remote_file_path} ..."
+  scp -i ~/.ssh/id_rsa_ya -r remote-alexander:${remote_file_path} ${WHERE_TO_KEEP}
+done
