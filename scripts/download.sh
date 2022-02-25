d5a6c3e9972791fe45ed8f82655e583b83624917
Artur Paniukov <chgk1101@gmail.com>
2022-02-25 18:27:24 +0300

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -5,6 +5,7 @@ source venv/bin/activate
 
 #HF_MODEL_NAME=lincoln/flaubert-mlsum-topic-classification
 #ONNX_MODEL_PATH=onnx_ph/
+#Check for this type of output from transformers.onnx: Some weights of the model checkpoint at katrin-kc/bert-finetuned-imdb were not used when initializing BertModel: ['classifier.bias', 'classifier.weight']
 
 python -m transformers.onnx \
           --model=${HF_MODEL_NAME} \
